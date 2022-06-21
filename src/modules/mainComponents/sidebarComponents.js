0df385a4c8ba4d6194abd2ddb6239c5a2caec5ac
Harold <cvuyante24@gmail.com>
2022-06-21 14:06:20 +0800

    June 21 2022 / Fourth Commit

--- src/modules/mainComponents/sidebarComponents.js
+++ src/modules/mainComponents/sidebarComponents.js
@@ -1,18 +1,30 @@
 import '../../styles/main.scss';
 
+const objItem = [
+	{ icon: 'fa-inbox', title: 'Inbox' },
+	{ icon: 'fa-calendar-check', title: 'Today' },
+	{ icon: 'fa-calendar-alt', title: 'This Week' }
+];
+
+const arrItem = ['Inbox', 'Today', 'This Week'];
+
 export const sidebarComponents = () => {
-	const arrItem = ['Inbox', 'Today', 'This Week'];
 	const sidebarContainer = document.createElement('div');
 	const listItem = document.createElement('ul');
 
-	for (let i = 0; i < arrItem.length; i++) {
+	for (let i = 0; i < objItem.length; i++) {
 		const createList = document.createElement('li');
 
-		createList.innerHTML = arrItem[i];
+		createList.innerHTML = `
+			<i class="fas ${objItem[i].icon}"></i><a class='sideBarContainer__list--item__link' href='#'> ${objItem[i].title}</a>
+		`;
+		createList.classList.add('sideBarContainer__list--item');
 
 		listItem.appendChild(createList);
 	}
 
+	sidebarContainer.classList.add('sideBarContainer');
+	listItem.classList.add('sideBarContainer__list');
 	sidebarContainer.appendChild(listItem);
 
 	return sidebarContainer;
