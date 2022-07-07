b57662fd24effa4a931200a51e80233a279c4cee
Harold <cvuyante24@gmail.com>
2022-07-07 14:37:43 +0800

    fourteenth commit / july 07 2022

--- src/modules/contentComponents/inboxIndex.js
+++ src/modules/contentComponents/inboxIndex.js
@@ -4,24 +4,24 @@ import { format, endOfDay } from 'date-fns';
 const todayDate = format(endOfDay(new Date()), 'MM/dd/yyyy');
 
 const dataArr = [
-	{
-		inputId: 'testing',
-		inputName: 'testing',
-		inputContent: 'first paragraph',
-		inputDate: todayDate
-	},
-	{
-		inputId: 'testing2',
-		inputName: 'testing2',
-		inputContent: 'second paragraph',
-		inputDate: todayDate
-	},
-	{
-		inputId: 'testing3',
-		inputName: 'testing3',
-		inputContent: 'Third paragraph',
-		inputDate: todayDate
-	}
+	// {
+	// 	inputId: Math.floor(Math.random() * 999),
+	// 	inputName: 'testing',
+	// 	inputContent: 'first paragraph',
+	// 	inputDate: todayDate
+	// },
+	// {
+	// 	inputId: Math.floor(Math.random() * 999),
+	// 	inputName: 'testing2',
+	// 	inputContent: 'second paragraph',
+	// 	inputDate: todayDate
+	// },
+	// {
+	// 	inputId: Math.floor(Math.random() * 999),
+	// 	inputName: 'testing3',
+	// 	inputContent: 'Third paragraph',
+	// 	inputDate: todayDate
+	// }
 ];
 
 const setAttributes = (elem, attr) => {
@@ -30,30 +30,47 @@ const setAttributes = (elem, attr) => {
 	}
 };
 
+const getStore = () => {
+	const getStoreTask = JSON.parse(localStorage.getItem('storeTask'));
+
+	return getStoreTask;
+};
+
 const radioButtons = () => {
 	const element = document.createElement('div');
 
-	for (const data of dataArr) {
+	for (const data of getStore()) {
 		const elementWrapper = document.createElement('div');
 		const input = document.createElement('input');
 		const forInput = document.createElement('label');
 		const datePara = document.createElement('p');
 		const icon = document.createElement('i');
 
-		forInput.textContent = data.inputContent;
-		datePara.textContent = data.inputDate;
+		elementWrapper.classList.add('contentContainer__input');
+		input.classList.add('contentContainer__input--checkBox');
+		forInput.classList.add('contentContainer__input--title');
+		datePara.classList.add('contentContainer__input--date');
+
 		setAttributes(input, {
 			type: 'checkbox',
 			id: data.inputId,
 			name: data.inputName
 		});
-		setAttributes(icon, { class: 'fas fa-trash' });
+		setAttributes(icon, {
+			class: 'contentContainer__input--icon fas fa-trash'
+		});
+		forInput.textContent = data.inputContent;
+		datePara.textContent = data.inputDate;
 
-		elementWrapper.classList.add('contentContainer__input');
-		input.classList.add('contentContainer__input--checkBox');
-		forInput.classList.add('contentContainer__input--title');
-		datePara.classList.add('contentContainer__input--date');
-		icon.classList.add('contentContainer__input--icon');
+		icon.addEventListener('click', () => {
+			const currentTask = getStore();
+			const newTask = currentTask.filter(
+				(task) => task.inputId != data.inputId
+			);
+
+			localStorage.setItem('storeTask', JSON.stringify(newTask));
+			location.reload();
+		});
 
 		elementWrapper.appendChild(input);
 		elementWrapper.appendChild(forInput);
@@ -87,15 +104,22 @@ const modalTask = () => {
 
 	confirmBtn.addEventListener('click', () => {
 		const inputVal = document.querySelector('#inputTask').value;
+		const currentTask = getStore();
 
-		const dataVal = dataArr.push({
+		dataArr.push({
 			inputId: Math.floor(Math.random() * 999),
 			inputName: 'testing',
 			inputContent: inputVal,
 			inputDate: todayDate
 		});
 
-		console.log(dataArr);
+		const lastArr = [dataArr.slice(-1).pop()];
+
+		localStorage.setItem(
+			'storeTask',
+			JSON.stringify(currentTask.concat(lastArr))
+		);
+		location.reload();
 
 		return dataVal;
 	});
