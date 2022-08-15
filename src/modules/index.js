cadefd1a1462c882ac802f9a4d8773ce44d4ddff
Harold <cvuyante24@gmail.com>
2022-08-15 14:36:52 +0800

    tweentythree commit / august 15 2022

--- src/modules/index.js
+++ src/modules/index.js
@@ -10,41 +10,82 @@ export const mainComponents = () => {
 	divWrapper.classList.add('wrapper');
 
 	divWrapper.appendChild(sidebarComponents());
+	// divWrapper.appendChild(inboxComponents());
 
+	// setTimeout(() => {
+	// 	/*
+	// 		- Put if/else in order to remove the first event then add the current event you want to input.
+	// 			- insert in AddEventListener the if/else method.
+	// 		- Problem is the refresh every it refresh it is remove the eventlisterner.
+	// 		- Remove the first Element then insert the new
+	// 		- Try to use async await method in event listener in order to not use reload method
+	// 		- Try Switch case method
+	// 	*/
+	// 	const inboxBtn = document.querySelector('#inboxId');
+	// 	const todayBtn = document.querySelector('#todayId');
+	// 	const weekBtn = document.querySelector('#weekId');
+
+	// 	inboxBtn.addEventListener('click', () => {
+	// 		divWrapper.appendChild(inboxComponents());
+	// 	});
+
+	// 	todayBtn.addEventListener('click', (event) => {
+	// 		document.location.reload();
+	// 		event.preventDefault();
+	// 		setTimeout(() => {
+	// 			divWrapper.appendChild(todayComponents());
+	// 		}, 3000);
+	// 	});
+
+	// 	weekBtn.addEventListener('click', () => {
+	// 		divWrapper.appendChild(weekComponents());
+	// 	});
+	// }, 1000);
+
+	// const removeAllElement = () => {
+	// 	const selectElement = document.querySelector('#inboxIndex');
+
+	// 	return selectElement.removeChild(selectElement.lastElementChild);
+	// };
+
+	// IS NOT WORKING FIX THE PARAMS AREA WHERE SEARCH THE SPECIFIC ID AND INSERT IT INTO PAGES
 	setTimeout(() => {
-		/*
-			- Put if/else in order to remove the first event then add the current event you want to input.
-				- insert in AddEventListener the if/else method.
-			- Problem is the refresh every it refresh it is remove the eventlisterner.
-			- Remove the first Element then insert the new
-			- Try to use async await method in event listener in order to not use reload method
-		*/
 		const inboxBtn = document.querySelector('#inboxId');
 		const todayBtn = document.querySelector('#todayId');
 		const weekBtn = document.querySelector('#weekId');
+		const params = document.querySelector('#inboxId');
 
-		inboxBtn.addEventListener('click', () => {
-			divWrapper.appendChild(inboxComponents());
-		});
+		switch (params) {
+			case '#inboxId':
+				inboxBtn.addEventListener(() => {
+					removeAllElement(inboxComponents());
+				});
+				break;
 
-		todayBtn.addEventListener('click', (event) => {
-			document.location.reload();
-			event.preventDefault();
+			case '#todayId':
+				todayBtn.addEventListener(() => {
+					removeAllElement(todayComponents());
+				});
+				break;
 
-			setTimeout(() => {
-				divWrapper.appendChild(todayComponents());
-			}, 3000);
-		});
+			case '#weekId':
+				weekBtn.addEventListener(() => {
+					removeAllElement(weekComponents());
+				});
+				break;
 
-		weekBtn.addEventListener('click', () => {
-			divWrapper.appendChild(weekComponents());
-		});
-	}, 1000);
+			default:
+				divWrapper.appendChild(inboxComponents());
+		}
+	}, 10);
 
-	const removeAllElement = () => {
+	const removeAllElement = (paramsComponents) => {
 		const selectElement = document.querySelector('#inboxIndex');
 
-		return selectElement.removeChild(selectElement.lastElementChild);
+		selectElement.parentElement.removeChild(selectElement);
+		divWrapper.appendChild(paramsComponents);
+
+		location.reload();
 	};
 
 	document.body.appendChild(headerComponents());
