1aea96a76daf2444bcdba98a6a91252a04fcd523
Harold <cvuyante24@gmail.com>
2022-09-22 14:40:26 +0800

    thirtyfive commit / sept 22 2022

--- src/modules/contentComponents/inboxIndex.js
+++ src/modules/contentComponents/inboxIndex.js
@@ -1,8 +1,25 @@
 import '../../styles/main.scss';
 import { format, endOfDay } from 'date-fns';
-import { getStore } from '../inboxComponents/getTask';
+import { getStore } from '../data/getTask';
 import { setAttributes } from '../helperComponents/setAttributes';
-import { radioButtons, getLastItem } from '../inboxComponents/radioButtons';
+
+/*	
+PROBLEM ::
+	If we delete it should double the delete data example
+	1. apple (if i clicked this one and deleted)
+	2. orange
+	3. banana
+	RESULT WILL :: 
+	1. apple (deleted)
+	2. orange
+	3. banana
+	2. orange 
+	3. banana
+	
+POSIBLE SOLUTION ::
+	- Check the await method if working it should remove the contentContainer 
+	and replace by new appendchild.
+*/
 
 const mainInboxComponents = (paramsTarget) => {
 	const dataArr = [];
@@ -44,6 +61,10 @@ const mainInboxComponents = (paramsTarget) => {
 				resolve(result);
 			}, 500);
 		});
+	};
+
+	const truncateResult = (data, size) => {
+		return data.length > size ? `${data.slice(0, size - 1)} ...` : data;
 	};
 
 	const modalTask = () => {
@@ -104,6 +125,102 @@ const mainInboxComponents = (paramsTarget) => {
 		elementTask.appendChild(cancelBtn);
 
 		return elementTask;
+	};
+
+	const getElement = (params1, params2) => {
+		const elementWrapper = document.createElement('div');
+		const firstDiv = document.createElement('div');
+		const secondDiv = document.createElement('div');
+		const input = document.createElement('input');
+		const forInput = document.createElement('label');
+		const datePara = document.createElement('p');
+		const icon = document.createElement('i');
+
+		setAttributes(elementWrapper, {
+			class: 'contentContainer__input'
+		});
+		setAttributes(firstDiv, {
+			class: 'contentContainer__input--first'
+		});
+		setAttributes(secondDiv, {
+			class: 'contentContainer__input--second'
+		});
+
+		setAttributes(input, {
+			class: ' contentContainer__input--first__checkBox',
+			type: 'checkbox',
+			id: params1.inputId,
+			name: params1.inputName
+		});
+		setAttributes(forInput, {
+			class: 'contentContainer__input--first__title'
+		});
+
+		setAttributes(datePara, {
+			class: ' contentContainer__input--second__date'
+		});
+		setAttributes(icon, {
+			class: 'contentContainer__input--second__icon fas fa-trash'
+		});
+
+		let dataInputContent = params1.inputContent;
+		let dataResult = truncateResult(dataInputContent, 50);
+
+		forInput.textContent = dataResult;
+		datePara.textContent = params1.inputDate;
+
+		input.addEventListener('click', () => {
+			forInput.classList.toggle('contentContainer__input--lineThrough');
+		});
+
+		icon.addEventListener('click', async () => {
+			const currentTask = getStore('storeTask');
+			const newTask = currentTask.filter(
+				(task) => task.inputId != params1.inputId
+			);
+
+			localStorage.setItem('storeTask', JSON.stringify(newTask));
+
+			await removeAddTask('#contentContainer__main');
+			paramsTarget.appendChild(radioButtons());
+			paramsTarget.appendChild(buttonWrapper);
+		});
+
+		elementWrapper.appendChild(firstDiv);
+		elementWrapper.appendChild(secondDiv);
+		firstDiv.appendChild(input);
+		firstDiv.appendChild(forInput);
+		secondDiv.appendChild(datePara);
+		secondDiv.appendChild(icon);
+
+		params2.appendChild(elementWrapper);
+	};
+
+	const radioButtons = () => {
+		const element = document.createElement('div');
+
+		setAttributes(element, {
+			id: 'contentContainer__main'
+		});
+
+		for (const data of getStore('storeTask')) {
+			getElement(data, element);
+		}
+
+		return element;
+	};
+
+	const getLastItem = () => {
+		const element = document.createElement('div');
+		const dataArr = getStore('storeTask');
+		const getLastItemArr = dataArr[dataArr.length - 1];
+
+		setAttributes(element, {
+			id: 'contentContainer__main'
+		});
+		getElement(getLastItemArr, element);
+
+		return element;
 	};
 
 	paramsTarget.appendChild(h1);
