ea3c9906d91c734b845a569e55ef2eaeddf64d62
Harold <cvuyante24@gmail.com>
2022-10-04 14:27:19 +0800

    fourtyone commit afternoon / oct 4 2022

--- src/modules/contentComponents/thisWeekIndex.js
+++ src/modules/contentComponents/thisWeekIndex.js
@@ -1,30 +1,117 @@
 import '../../styles/main.scss';
 import { format, endOfDay } from 'date-fns';
+import { getStore } from '../data/getTask';
+import { setAttributes } from '../helperComponents/setAttributes';
 
-const todayDate = format(endOfDay(new Date()), 'MM/dd/yyyy');
+const mainInboxComponents = (paramsTarget) => {
+	const dataArr = [];
+	const todayDate = format(endOfDay(new Date()), 'MM/dd/yyyy');
 
-const dataArr = [];
+	const buttonWrapper = document.createElement('div');
+	const h1 = document.createElement('h1');
+	const buttonTask = document.createElement('button');
+	const icon = document.createElement('i');
 
-const setAttributes = (elem, attr) => {
-	for (const key in attr) {
-		elem.setAttribute(key, attr[key]);
-	}
-};
+	setAttributes(h1, {
+		class: 'contentContainer__header'
+	});
+	setAttributes(buttonTask, {
+		class: 'contentContainer__addButton--button'
+	});
+	setAttributes(icon, {
+		class: 'contentContainer__addButton--icon fas fa-plus-square'
+	});
+	setAttributes(buttonWrapper, {
+		class: 'contentContainer__addButton',
+		id: 'addBtnWrapper'
+	});
 
-const getStore = () => {
-	const getStoreTask = JSON.parse(localStorage.getItem('storeWeekTask'));
+	h1.textContent = 'This Week inbox';
+	buttonTask.textContent = 'Add Task';
+	buttonTask.addEventListener('click', () => {
+		const targetElement = document.querySelector('#addBtnWrapper');
 
-	return getStoreTask;
-};
+		paramsTarget.appendChild(modalTask());
+		targetElement.parentElement.removeChild(targetElement);
+	});
 
-const truncateResult = (data, size) => {
-	return data.length > size ? `${data.slice(0, size - 1)} ...` : data;
-};
+	const removeAddTask = (target) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				const targetNode = document.querySelector(target);
+				const result = targetNode.parentNode.removeChild(targetNode);
+				resolve(result);
+			}, 500);
+		});
+	};
 
-const radioButtons = () => {
-	const element = document.createElement('div');
+	const truncateResult = (data, size) => {
+		return data.length > size ? `${data.slice(0, size - 1)} ...` : data;
+	};
 
-	for (const data of getStore()) {
+	const modalTask = () => {
+		const elementTask = document.createElement('div');
+		const inputTask = document.createElement('input');
+		const confirmBtn = document.createElement('button');
+		const cancelBtn = document.createElement('button');
+
+		setAttributes(elementTask, {
+			class: 'contentContainer__task',
+			id: 'modalTask'
+		});
+		setAttributes(inputTask, {
+			type: 'text',
+			id: 'inputTask',
+			name: 'Input Task',
+			class: 'contentContainer__task--input'
+		});
+		setAttributes(confirmBtn, { class: 'contentContainer__task--confirm' });
+		setAttributes(cancelBtn, { class: 'contentContainer__task--cancel' });
+
+		confirmBtn.textContent = 'Confirm';
+		cancelBtn.textContent = 'Cancel';
+
+		confirmBtn.addEventListener('click', async () => {
+			const inputVal = document.querySelector('#inputTask').value;
+			const currentTask = getStore('storeWeekTask');
+
+			if (inputVal === null || inputVal === '') {
+				alert('input some text');
+			} else {
+				dataArr.push({
+					inputId: Math.floor(Math.random() * 999),
+					inputName: 'testing',
+					inputContent: inputVal,
+					inputDate: todayDate
+				});
+
+				const lastArr = [dataArr.slice(-1).pop()];
+
+				localStorage.setItem(
+					'storeWeekTask',
+					JSON.stringify(currentTask.concat(lastArr))
+				);
+				await removeAddTask('#modalTask');
+				await removeAddTask('#contentContainer__main');
+				// paramsTarget.appendChild(getLastItem());
+				paramsTarget.appendChild(showAllItem());
+				paramsTarget.appendChild(buttonWrapper);
+			}
+		});
+
+		cancelBtn.addEventListener('click', async () => {
+			await removeAddTask('#modalTask');
+			paramsTarget.appendChild(buttonWrapper);
+		});
+
+		elementTask.appendChild(inputTask);
+		elementTask.appendChild(confirmBtn);
+		elementTask.appendChild(cancelBtn);
+
+		return elementTask;
+	};
+
+	const getElement = (params1, params2) => {
 		const elementWrapper = document.createElement('div');
 		const firstDiv = document.createElement('div');
 		const secondDiv = document.createElement('div');
@@ -42,17 +129,15 @@ const radioButtons = () => {
 		setAttributes(secondDiv, {
 			class: 'contentContainer__input--second'
 		});
-
 		setAttributes(input, {
 			class: ' contentContainer__input--first__checkBox',
 			type: 'checkbox',
-			id: data.inputId,
-			name: data.inputName
+			id: params1.inputId,
+			name: params1.inputName
 		});
 		setAttributes(forInput, {
 			class: 'contentContainer__input--first__title'
 		});
-
 		setAttributes(datePara, {
 			class: ' contentContainer__input--second__date'
 		});
@@ -60,24 +145,27 @@ const radioButtons = () => {
 			class: 'contentContainer__input--second__icon fas fa-trash'
 		});
 
-		let dataInputContent = data.inputContent;
+		let dataInputContent = params1.inputContent;
 		let dataResult = truncateResult(dataInputContent, 50);
 
 		forInput.textContent = dataResult;
-		datePara.textContent = data.inputDate;
+		datePara.textContent = params1.inputDate;
 
 		input.addEventListener('click', () => {
 			forInput.classList.toggle('contentContainer__input--lineThrough');
 		});
 
-		icon.addEventListener('click', () => {
-			const currentTask = getStore();
+		icon.addEventListener('click', async () => {
+			const currentTask = getStore('storeWeekTask');
 			const newTask = currentTask.filter(
-				(task) => task.inputId != data.inputId
+				(task) => task.inputId != params1.inputId
 			);
 
 			localStorage.setItem('storeWeekTask', JSON.stringify(newTask));
-			location.reload();
+
+			await removeAddTask('#contentContainer__main');
+			paramsTarget.appendChild(showAllItem());
+			paramsTarget.appendChild(buttonWrapper);
 		});
 
 		elementWrapper.appendChild(firstDiv);
@@ -87,115 +175,39 @@ const radioButtons = () => {
 		secondDiv.appendChild(datePara);
 		secondDiv.appendChild(icon);
 
-		element.appendChild(elementWrapper);
-	}
+		params2.appendChild(elementWrapper);
+	};
 
-	return element;
+	const showAllItem = () => {
+		const element = document.createElement('div');
+		setAttributes(element, {
+			id: 'contentContainer__main'
+		});
+
+		for (const data of getStore('storeWeekTask')) {
+			getElement(data, element);
+		}
+
+		console.log(element);
+		return element;
+	};
+
+	paramsTarget.appendChild(h1);
+	paramsTarget.appendChild(showAllItem());
+	buttonWrapper.appendChild(icon);
+	buttonWrapper.appendChild(buttonTask);
+	paramsTarget.appendChild(buttonWrapper);
 };
 
 export const weekComponents = () => {
 	const element = document.createElement('div');
-	const buttonWrapper = document.createElement('div');
-	const h1 = document.createElement('h1');
-	const buttonTask = document.createElement('button');
-	const icon = document.createElement('i');
-
-	h1.textContent = 'This Week inbox';
-	buttonTask.textContent = 'Add Task';
-	buttonTask.addEventListener('click', () => {
-		const targetElement = document.querySelector('#addBtnWrapper');
-
-		element.appendChild(modalTask());
-		targetElement.parentElement.removeChild(targetElement);
-	});
 
 	setAttributes(element, {
 		class: 'contentContainer',
-		id: 'weekIndex'
-	});
-	setAttributes(h1, {
-		class: 'contentContainer__header'
-	});
-	setAttributes(buttonTask, {
-		class: 'contentContainer__addButton--button'
-	});
-	setAttributes(icon, {
-		class: 'contentContainer__addButton--icon fas fa-plus-square'
-	});
-	setAttributes(buttonWrapper, {
-		class: 'contentContainer__addButton',
-		id: 'addBtnWrapper'
+		id: 'inboxIndex'
 	});
 
-	const modalTask = () => {
-		const element = document.createElement('div');
-		const inputTask = document.createElement('input');
-		const confirmBtn = document.createElement('button');
-		const cancelBtn = document.createElement('button');
-
-		setAttributes(element, {
-			class: 'contentContainer__task',
-			id: 'modalTask'
-		});
-		setAttributes(inputTask, {
-			type: 'text',
-			id: 'inputTask',
-			name: 'Input Task',
-			class: 'contentContainer__task--input'
-		});
-		setAttributes(confirmBtn, { class: 'contentContainer__task--confirm' });
-		setAttributes(cancelBtn, { class: 'contentContainer__task--cancel' });
-
-		confirmBtn.textContent = 'Confirm';
-		cancelBtn.textContent = 'Cancel';
-
-		confirmBtn.addEventListener('click', () => {
-			const inputVal = document.querySelector('#inputTask').value;
-			const currentTask = getStore();
-
-			dataArr.push({
-				inputId: Math.floor(Math.random() * 999),
-				inputName: 'testing',
-				inputContent: inputVal,
-				inputDate: todayDate
-			});
-
-			const lastArr = [dataArr.slice(-1).pop()];
-
-			if (inputVal === null || inputVal === '') {
-				alert('input some text');
-			} else {
-				localStorage.setItem(
-					'storeWeekTask',
-					JSON.stringify(currentTask.concat(lastArr))
-				);
-
-				element.appendChild(buttonWrapper);
-				location.reload();
-			}
-		});
-
-		cancelBtn.addEventListener('click', () => {
-			const targetElement = document.querySelector('#modalTask');
-
-			targetElement.parentElement.removeChild(targetElement);
-			element.appendChild(buttonWrapper);
-
-			location.reload();
-		});
-
-		element.appendChild(inputTask);
-		element.appendChild(confirmBtn);
-		element.appendChild(cancelBtn);
-
-		return element;
-	};
-
-	element.appendChild(h1);
-	element.appendChild(radioButtons());
-	buttonWrapper.appendChild(icon);
-	buttonWrapper.appendChild(buttonTask);
-	element.appendChild(buttonWrapper);
+	mainInboxComponents(element);
 
 	return element;
 };
