7a615f62257c4c3d1410708e06cc2bd377bef0bd
Harold <cvuyante24@gmail.com>
2022-10-17 14:45:54 +0800

    Add SideContainer Features / oct 17 2022

--- src/modules/mainComponents/sidebarComponents.js
+++ src/modules/mainComponents/sidebarComponents.js
@@ -1,5 +1,4 @@
 import '../../styles/main.scss';
-import { inboxComponents } from '../contentComponents/inboxIndex';
 
 const setAttributes = (elem, attr) => {
 	for (const key in attr) {
@@ -13,9 +12,50 @@ const objItem = [
 	{ icon: 'fa-calendar-alt', title: 'This Week', id: 'weekId' }
 ];
 
+const addProject = (paramTarget) => {
+	const subHeadWrapper = document.createElement('div');
+	const titleSubHead = document.createElement('h1');
+	const icon = document.createElement('i');
+	const addBtn = document.createElement('button');
+
+	setAttributes(titleSubHead, {
+		class: 'sideBarContainer__subHeading'
+	});
+	setAttributes(subHeadWrapper, { class: 'sidebarContainer__Wrapper' });
+	setAttributes(icon, {
+		class: 'sideBarContainer__Wrapper--icon fas fa-plus-square'
+	});
+	setAttributes(addBtn, {
+		class: 'sideBarContainer__Wrapper--btn'
+	});
+
+	titleSubHead.textContent = 'Project';
+	addBtn.textContent = 'Add Project Test';
+
+	subHeadWrapper.addEventListener('click', () => {
+		console.log(`just clicked`);
+	});
+
+	subHeadWrapper.appendChild(icon);
+	subHeadWrapper.appendChild(addBtn);
+
+	return (
+		paramTarget.appendChild(titleSubHead) &&
+		paramTarget.appendChild(subHeadWrapper)
+	);
+};
+
 export const sidebarComponents = () => {
 	const sidebarContainer = document.createElement('div');
 	const listItem = document.createElement('ul');
+
+	setAttributes(sidebarContainer, {
+		class: 'sideBarContainer'
+	});
+	setAttributes(listItem, {
+		class: 'sideBarContainer__list',
+		id: 'sideBarContainer'
+	});
 
 	for (let i = 0; i < objItem.length; i++) {
 		const createList = document.createElement('li');
@@ -26,29 +66,16 @@ export const sidebarComponents = () => {
 
 		setAttributes(createList, {
 			class: 'sideBarContainer__list--item'
-			// id: 'sideBarContainer'
 		});
 
 		setAttributes(createButton, { id: objItem[i].id });
-
-		// createButton.addEventListener('click', () => {
-		// 	document.body.append(inboxComponents());
-		// 	console.log('clicked');
-		// });
 
 		createList.appendChild(createButton);
 		listItem.appendChild(createList);
 	}
 
-	setAttributes(sidebarContainer, {
-		class: 'sideBarContainer'
-	});
-	setAttributes(listItem, {
-		class: 'sideBarContainer__list',
-		id: 'sideBarContainer'
-	});
-
 	sidebarContainer.appendChild(listItem);
+	sidebarContainer.appendChild(addProject(sidebarContainer));
 
 	return sidebarContainer;
 };
